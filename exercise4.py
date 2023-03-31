b7a4dc422bb6923588be9556a0e1310def0a03bc
sanaxamjad <98844696+sanaxamjad@users.noreply.github.com>
2023-03-31 02:14:12 -0400

    Initial Commit 4 with dosctrings Exercise 4

--- exercise4.py
+++ exercise4.py
@@ -3,10 +3,20 @@ Driver: Sana Amjad, Pride Kwekam
 Navigator: Sana Amjad, Pride Kwekam
 Assignment: INST326 - Exercise 4
 Date: 03_31_23
+
+This program defines two functions: say_hello() and say_goodbye().
 """
 
-#Exercise 4
 def say_hello(name):
+    """
+    Takes a name as an argument and returns a greeting string.
+    
+    Parameters:
+    name (str): The name of the person to greet.
+    
+    Returns:
+    str: A greeting string that includes the person's name.
+    """
     greeting = "Hello, " + name + "!"
     return greeting
 
@@ -14,6 +24,15 @@ message = say_hello("Sana")
 print(message)
 
 def say_goodbye():
+    """
+    Prints a goodbye message to the console.
+    
+    Parameters:
+    None
+    
+    Returns:
+    None
+    """
     print("goodbye")
 
 say_goodbye()
